fb34b509c7b908bff5b60ecc0d64088a8c5d1843
GerganaRoeva <gergana.roeva@gmail.com>
2022-01-17 22:59:10 +0200

    menu and board, buttons for number of players

--- main.py
+++ main.py
@@ -11,26 +11,46 @@ winWidth = 600
 
 screen = pygame.display.set_mode([winHeight, winWidth])
 
-board = pygame.image.load(os.path.join('Assets', 'board.jpg'))
+board = pygame.image.load(os.path.join('Assets', 'board.png'))
 board = pygame.transform.scale(board, (winHeight, winWidth))
 
+menu = pygame.image.load(os.path.join('Assets', 'menu.png'))
+menu = pygame.transform.scale(menu, (winHeight, winWidth))
+
+
+
 die = 0 # initial val 
+gamestart = False
+
+class Player:
+    def __init__(self, id):
+        self.id = id
+
 
 def draw():
-    screen.blit(board, (0, 0))
+    if not gamestart:
+        screen.blit(menu, (0, 0))
+        two_players_button.show_button(screen, colors.black, fonts.calibri)
+        three_players_button.show_button(screen, colors.black, fonts.calibri)
+        four_players_button.show_button(screen, colors.black, fonts.calibri)
 
-    die_button.show_button(screen, colors.black, smaller_corbel)
+    else:
+        screen.blit(board, (0, 0))
+        die_button.show_button(screen, colors.black, smaller_corbel)
 
-    if die:
-        dietxt = 'dice' + str(die) + '.png'
-        die_img = pygame.image.load(os.path.join('Assets', dietxt))
-        die_img = pygame.transform.scale(die_img, (50, 50))
-        screen.blit(die_img, (240, 275))
+        if die:
+            dietxt = 'dice' + str(die) + '.png'
+            die_img = pygame.image.load(os.path.join('Assets', dietxt))
+            die_img = pygame.transform.scale(die_img, (50, 50))
+            screen.blit(die_img, (240, 275))
+
+    
 
 
 def run():
 
     global die
+    global gamestart
 
     running = True
     while running:
@@ -47,6 +67,28 @@ def run():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if die_button.is_hovering(mouse_pos):
                     die = random.randint(1, 6)
+
+            if not gamestart:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    player1 = Player(1)
+                    player2 = Player(2)
+                    if two_players_button.is_hovering(mouse_pos):
+                        players = [player1, player2]
+                        gamestart = True
+                        screen.blit(board, (0, 0))
+                    if three_players_button.is_hovering(mouse_pos):
+                        player3 = Player(3)
+                        players = [player1, player2, player3]
+                        gamestart = True
+                        screen.blit(board, (0, 0))
+                    if four_players_button.is_hovering(mouse_pos):
+                        player3 = Player(3)
+                        player4 = Player(4)
+                        players = [player1, player2, player3, player4]
+                        gamestart = True
+                        screen.blit(board, (0, 0))
+
+                
 
         pygame.display.update()
 
