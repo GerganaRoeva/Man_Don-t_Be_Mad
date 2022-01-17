0f86628fb44d5174d3b079b3c32296b2b78f20e6
GerganaRoeva <37037382+GerganaRoeva@users.noreply.github.com>
2022-01-17 23:19:16 +0200

    Choose number players (#12)
    * added button class, fronts & colors
    
    * small changes
    
    * small fonts

--- main.py
+++ main.py
@@ -17,10 +17,9 @@ board = pygame.transform.scale(board, (winHeight, winWidth))
 menu = pygame.image.load(os.path.join('Assets', 'menu.png'))
 menu = pygame.transform.scale(menu, (winHeight, winWidth))
 
-
-
 die = 0 # initial val 
 gamestart = False
+
 
 class Player:
     def __init__(self, id):
@@ -29,10 +28,9 @@ class Player:
 
 def draw():
     if not gamestart:
-        screen.blit(menu, (0, 0))
-        two_players_button.show_button(screen, colors.black, fonts.calibri)
-        three_players_button.show_button(screen, colors.black, fonts.calibri)
-        four_players_button.show_button(screen, colors.black, fonts.calibri)
+        two_players_button.show_button(screen, colors.black, small_calibri)
+        three_players_button.show_button(screen, colors.black, small_calibri)
+        four_players_button.show_button(screen, colors.black, small_calibri)
 
     else:
         screen.blit(board, (0, 0))
@@ -46,23 +44,41 @@ def draw():
 
     
 
-
 def run():
 
     global die
     global gamestart
+    playing = True
 
-    running = True
-    while running:
+
+    while playing:
         pygame.time.Clock().tick(60)
         draw()
         mouse_pos = pygame.mouse.get_pos()
-
+        
         for event in pygame.event.get():
+            if not gamestart:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    player1 = Player(1)
+                    player2 = Player(2)
+                    if two_players_button.is_hovering(mouse_pos):
+                        players = [player1, player2]
+                    if three_players_button.is_hovering(mouse_pos):
+                        player3 = Player(3)
+                        players = [player1, player2, player3]
+                    if four_players_button.is_hovering(mouse_pos):
+                        player3 = Player(3)
+                        player4 = Player(4)
+                        players = [player1, player2, player3, player4]
+                    gamestart = True
+
+
+        
             mouse_hovering(event, mouse_pos)
 
             if event.type == pygame.QUIT:
-                running = False
+                playing = False
+
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if die_button.is_hovering(mouse_pos):
