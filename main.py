93436add18108d98121aeeedae8a4f731748900d
Ivan-Enchev <72374922+Ivan-Enchev@users.noreply.github.com>
2022-04-17 23:25:11 +0300

    Remove pawns (#18)
    * added pawns
    
    * Added basic movement
    
    * Pawns can now finish the cycle
    
    * Update main.py
    
    Co-authored-by: GerganaRoeva <37037382+GerganaRoeva@users.noreply.github.com>

--- main.py
+++ main.py
@@ -66,8 +66,33 @@ def draw():
             die_img = pygame.image.load(os.path.join('Assets', dietxt))
             die_img = pygame.transform.scale(die_img, (50, 50))
             screen.blit(die_img, (240, 275))
+            if die == 6:
+                move_button.show_button(screen, colors.black, smaller_comic_sans)
+                spawn_button.show_button(screen, colors.black, smaller_comic_sans)
 
     
+def finish_pawn_check(pawn):
+    if players[current_player - 1].id == 2:
+        if pawn.pos[0] <= 10 and pawn.pos[1] <= 10:
+            pawns.remove(pawn)
+    elif players[current_player - 1].id == 1:
+        if pawn.pos[0] >= 540 and pawn.pos[1] >= 540:
+            pawns.remove(pawn)   
+    elif players[current_player - 1].id == 4:
+        if pawn.pos[0] >= 540 and pawn.pos[1] <= 10:
+            pawns.remove(pawn) 
+    elif players[current_player - 1].id == 3:
+        if pawn.pos[0] <= 10 and pawn.pos[1] >= 540:
+            pawns.remove(pawn)      
+
+def check_player():
+    counter = 0
+    for pawn in pawns:
+        if pawn.player == players[current_player - 1].id:
+            counter += 1
+    if counter == 0:
+        players.remove(players[current_player - 1])
+
 
 def run():
 
@@ -93,6 +118,91 @@ def run():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if die_button.is_hovering(mouse_pos):
                     die = random.randint(1, 6)
+                    if die == 6:
+                        for pawn in pawns:
+                            # if event.type == pygame.MOUSEBUTTONDOWN:
+                            #     if spawn_button.is_hovering(mouse_pos):
+                            #         print("pressed spawn")
+                                    if pawn.player == players[current_player - 1].id and not pawn.active:
+                                        if players[current_player - 1].id == 1:
+                                            pawn.pos = (540, 540)
+                                            pawn.active = True
+                                            break
+                                        elif players[current_player - 1].id == 2:
+                                            pawn.pos = (10, 10)
+                                            pawn.active = True
+                                            break
+                                        elif players[current_player - 1].id == 3:
+                                            pawn.pos = (10, 540)
+                                            pawn.active = True
+                                            break
+                                        elif players[current_player - 1].id == 4:
+                                            pawn.pos = (540, 10)
+                                            pawn.active = True
+                                            break
+                            # if event.type == pygame.MOUSEBUTTONDOWN:
+                            #     if move_button.is_hovering(mouse_pos):
+                            #         print("pressed move")
+                            #         for i in range (0, 6):
+                            #             for pawn in pawns:
+                            #                 if pawn.player == players[current_player - 1].id and pawn.active:
+                            #                     if pawn.pos[0] > 10 and pawn.pos[1] >= 540:
+                            #                         new_pos = (pawn.pos[0] - 76 , pawn.pos[1])
+                            #                         pawn.pos = new_pos
+                            #                         print(pawn.pos)
+                            #                         finish_pawn_check(pawn)  
+                            #                         break
+                            #                     if pawn.pos[0] <= 10 and pawn.pos[1] > 10:
+                            #                         new_pos = (pawn.pos[0], pawn.pos[1] - 76)
+                            #                         pawn.pos = new_pos
+                            #                         print(pawn.pos)
+                            #                         finish_pawn_check(pawn)  
+                            #                         break
+                            #                     if pawn.pos[0] < 540 and pawn.pos[1] <= 10 :
+                            #                         new_pos = (pawn.pos[0] + 76 , pawn.pos[1])
+                            #                         pawn.pos = new_pos
+                            #                         print(pawn.pos)
+                            #                         finish_pawn_check(pawn)  
+                            #                         break
+                            #                     if pawn.pos[0] >= 540 and pawn.pos[1] < 540:
+                            #                         new_pos = (pawn.pos[0], pawn.pos[1] + 76)
+                            #                         pawn.pos = new_pos
+                            #                         print(pawn.pos)
+                            #                         finish_pawn_check(pawn)  
+                            #                         break
+                                    
+                    else:
+                        for i in range (0, die):
+                            for pawn in pawns:
+                                if pawn.player == players[current_player - 1].id and pawn.active:
+                                    if pawn.pos[0] > 10 and pawn.pos[1] >= 540:
+                                        new_pos = (pawn.pos[0] - 76 , pawn.pos[1])
+                                        pawn.pos = new_pos
+                                        print(pawn.pos)
+                                        finish_pawn_check(pawn)  
+                                        break
+                                    if pawn.pos[0] <= 10 and pawn.pos[1] > 10:
+                                        new_pos = (pawn.pos[0], pawn.pos[1] - 76)
+                                        pawn.pos = new_pos
+                                        print(pawn.pos)
+                                        finish_pawn_check(pawn)  
+                                        break
+                                    if pawn.pos[0] < 540 and pawn.pos[1] <= 10 :
+                                        new_pos = (pawn.pos[0] + 76 , pawn.pos[1])
+                                        pawn.pos = new_pos
+                                        print(pawn.pos)
+                                        finish_pawn_check(pawn)  
+                                        break
+                                    if pawn.pos[0] >= 540 and pawn.pos[1] < 540:
+                                        new_pos = (pawn.pos[0], pawn.pos[1] + 76)
+                                        pawn.pos = new_pos
+                                        print(pawn.pos)
+                                        finish_pawn_check(pawn)  
+                                        break
+                        check_player()
+                        current_player += 1
+                        if current_player > len(players):
+                            current_player = 1
 
             if not gamestart:
                 if event.type == pygame.MOUSEBUTTONDOWN:
